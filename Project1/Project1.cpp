16edd4138043c4367d072b7a3cab7075ac60634f
lucassimaoo <tortim.lucas@gmail.com>
2019-11-18 20:17:50 -0800

    separating the tests in different xmls

--- Project1/Project1.cpp
+++ Project1/Project1.cpp
@@ -25,21 +25,21 @@ int main()
 
 	Message msg1;
 	msg1.to(serverEP);
-	msg1.command("tests.xml");
+	msg1.command("tests1.xml");
 	msg1.name("client #1 : msg # 1");
 	std::cout << "\n  " + comm.name() + " posting:  " << msg1.name();
 	comm.postMessage(msg1);
 
 	Message msg2;
 	msg2.to(serverEP);
-	msg2.command("tests.xml");
+	msg2.command("tests2.xml");
 	msg2.name("client #1 : msg # 2");
 	std::cout << "\n  " + comm.name() + " posting:  " << msg2.name();
 	comm.postMessage(msg2);
 
 	Message msg3;
 	msg3.to(serverEP);
-	msg3.command("tests.xml");
+	msg3.command("tests3.xml");
 	msg3.name("client #1 : msg # 3");
 	std::cout << "\n  " + comm.name() + " posting:  " << msg3.name();
 	comm.postMessage(msg3);
